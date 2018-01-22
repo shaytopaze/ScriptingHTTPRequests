9ed65b3bfa85cead9f96f932945e0af8b58bd63a
Shay Alexander <shaytopazehair@gmail.com>
2018-01-22 19:43:57 +0000

    created getAndPrintHTML.js function - pushed all data chunks into a single variable to output once all data chunks were received

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -1,26 +1,29 @@
 var https = require('https');
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
+
+  var allData;
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
   https.get(requestOptions, function (response) {
 
   response.setEncoding('utf8');
 
-  response.on('data', function (data) {
+  allData += response.on('data', function (data) {
     console.log('Chunk Received.' + data + '\n');
   });
 
   response.on('end', function() {
-    console.log('Response stream complete.');
+    console.log('Response Stream Complete');
   });
+
+
  });
 
 }
 
-getAndPrintHTMLChunks();
-
+getAndPrintHTML();
